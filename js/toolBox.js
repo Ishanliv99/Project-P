bec1d4c79ed346a7e2a86c609b2d10aced315882
ishan <ishanliv99@gmail.com>
2017-11-30 10:19:00 +0545

    final

--- js/toolBox.js
+++ js/toolBox.js
@@ -9,7 +9,7 @@ class ToolBox {
     // this.pick = new Pick();
 
     this.currentTool = this.pencil;
-    // this.currentTool.element.style.backgroundColor = '#06A0D8';
+    this.currentTool.element.style.backgroundColor = '#06A0D8';
 
     panelLeft.appendChild(this.pencil.element);
     panelLeft.appendChild(this.eraser.element);
@@ -20,38 +20,48 @@ class ToolBox {
     // panelLeft.appendChild(this.pick.element);
 
     this.pencil.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.pencil;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
 
     this.eraser.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.eraser;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
 
     this.fillall.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.fillall;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
 
     this.spray.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.spray;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
 
     this.text.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.text;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
 
     this.rect.element.onclick = (event) => {
+      this.resetToolBox();
       this.currentTool = this.rect;
-      // this.currentTool.element.style.backgroundColor = '#06A0D8';
+      this.currentTool.element.style.backgroundColor = '#06A0D8';
     }
+  }
 
-    // if (this.pencil ==  this.currentTool)
-    //   this.pencil.element.style.backgroundColor = '#06A0D8';
-    // else
-    //   this.pencil.element.style.backgroundColor = '#FFF';
+  resetToolBox(){
+    this.pencil.element.style.backgroundColor = '#FFF';
+    this.eraser.element.style.backgroundColor = '#FFF';
+    this.fillall.element.style.backgroundColor = '#FFF';
+    this.spray.element.style.backgroundColor = '#FFF';
+    this.text.element.style.backgroundColor = '#FFF';
+    this.rect.element.style.backgroundColor = '#FFF';
   }
 }
